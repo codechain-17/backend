ddc0509f6af8dae9ba4cac7a825f79281c296c0e
nosemiusername <rafael@imatec.cl>
2021-12-15 05:20:10 -0300

    :bug: fixing id

--- src/models/job.js
+++ src/models/job.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const jobSchema = new mongoose.Schema({
     user: { username: String },
-    jobs: [{ id: Number }],
+    jobs: [{ id: String }],
 });
 
 export const Job = mongoose.model('Job', jobSchema);
